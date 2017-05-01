5974c220c8cfc198c14a15f28e46a747ad9761b0
student5 <student3@jh-mba158-14.d214.org>
2017-05-01 10:02:50 -0500

    Added first two chemistry equations on ViewController.swift, should probably move them to the right view controller

--- MobileAppsFinal/ViewController.swift
+++ MobileAppsFinal/ViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+var Equations = [Equation]()
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    super.viewDidLoad()
+    
+        let equationOne = Equation(equiName: "Density", equation: "D M/V")
+        let equationTwo = Equation(equiName: "% Yield", equation: "(Acual Yield/theoretical yield)X100%")
+        
+       
     }
 
  
