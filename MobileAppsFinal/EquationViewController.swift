b27ae2a461eea06576aa60c32e647196088557ab
student5 <student3@jh-mba158-14.d214.org>
2017-05-01 10:05:12 -0500

    Moved chemistry equations to the equationsViewcontroller, the proper place.

--- MobileAppsFinal/EquationViewController.swift
+++ MobileAppsFinal/EquationViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class EquationViewController: UIViewController {
-
+var Equations = [Equation]()
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let equationOne = Equation(equiName: "Density", equation: "D M/V")
+        let equationTwo = Equation(equiName: "% Yield", equation: "(Acual Yield/theoretical yield)X100%")
+        
+        
     }
     
 }
--- MobileAppsFinal/ViewController.swift
+++ MobileAppsFinal/ViewController.swift
@@ -9,14 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-var Equations = [Equation]()
+
     
     override func viewDidLoad() {
     super.viewDidLoad()
     
-        let equationOne = Equation(equiName: "Density", equation: "D M/V")
-        let equationTwo = Equation(equiName: "% Yield", equation: "(Acual Yield/theoretical yield)X100%")
-        
+       
        
     }
 
