ecb90a3254ae5bbfa4854c0ccf93edf6c2d159b1
student5 <student3@jh-mba158-02.d214.org>
2017-05-10 10:01:40 -0500

    finally got the whole table working, should display the classes and things

--- MobileAppsFinal/ViewController.swift
+++ MobileAppsFinal/ViewController.swift
@@ -8,17 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView1: UITableView!
+    
+    var Subjects = [Subject]()
     
     override func viewDidLoad() {
-    super.viewDidLoad()
-    
-       
-       
+        super.viewDidLoad()
+        let subjectOne = Subject(subject: "Chemistry", equations: [Equation()])
+        let subjectTwo = Subject(subject: "Biology", equations: [Equation()])
+        let subjectThree = Subject(subject: "Physics", equations: [Equation()])
+        
+        Subjects = [subjectOne, subjectTwo, subjectThree]
     }
-
- 
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return Subjects.count
+    }
+   
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "subjectCell")
+        let currentSubject = Subjects[indexPath.row]
+        cell?.textLabel?.text = currentSubject.subject
+        return cell!
+    }
 
 
 }
--- classFile.swift
+++ classFile.swift
@@ -9,7 +9,7 @@
 import Foundation
 import UIKit
 
-class Class {
+class Subject {
     var subject:String!
     var equations: [Equation]
     
