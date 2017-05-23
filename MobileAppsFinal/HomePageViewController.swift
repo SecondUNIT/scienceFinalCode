c79cb70085cc8b2e9ff4dbc9f11bc02ba2985572
student5 <student3@jh-mba158-16.d214.org>
2017-05-23 09:48:36 -0500

    Fixed Segues Added Periodic Table Button Hidey Thingy Made it work Removed Herobrine

--- MobileAppsFinal/HomePageViewController.swift
+++ MobileAppsFinal/HomePageViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class HomePageViewController: UIViewController {
     
     @IBOutlet weak var subjectLabel: UILabel!
+    @IBOutlet weak var periodicTableButton: UIButton!
+    
     //Buttons will be actions (change button attributes)
     var selectedSubject = Subject()
 
@@ -19,13 +21,30 @@ class HomePageViewController: UIViewController {
         
         navigationItem.title = selectedSubject.name
         subjectLabel.text = selectedSubject.name
-        
+        if selectedSubject.name == "Biology"
+        {
+            periodicTableButton.isEnabled = false
+            periodicTableButton.alpha = 0
+        }
+        else if selectedSubject.name == "Physics"
+        {
+            periodicTableButton.isEnabled = false
+            periodicTableButton.alpha = 0
+        }
+        else {
+            periodicTableButton.isEnabled = true
+            periodicTableButton.alpha = 1
+        }
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let nvc = segue.destination as! EquationViewController
-        
-        nvc.passedSubject = selectedSubject
+        if periodicTableButton.isTouchInside == true{
+            let nvc = segue.destination as!ChemViewController
+        }
+        else {
+            let nvc = segue.destination as!EquationViewController
+        }
+//        nvc.passedSubject = selectedSubject
         
     }
 }
